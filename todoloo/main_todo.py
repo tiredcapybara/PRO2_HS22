bcfa58112d43f11620470c3d8663d6308aaacf4e
tiredcapybara <113971222+tiredcapybara@users.noreply.github.com>
2022-11-11 09:37:56 +0100

    Jinja Header und Footer Projekt

--- todoloo/main_todo.py
+++ todoloo/main_todo.py
@@ -3,14 +3,21 @@ from flask import render_template
 from flask import request
 
 from todoloo.datenbank import abspeichern
+from todoloo.datenbank import auslesen
 
 app = Flask("todoloo")
 
 
 @app.route("/")
 def start():
-    return "ok"
-
+    todos = auslesen()
+    todos_html = todos.replace("\n", "<br>")
+    todo_liste = todos.split("\n")
+    neue_liste = []
+    for eintrag in todo_liste:
+        aufgabe, deadline = eintrag.split(",")
+        neue_liste.append([aufgabe, deadline])
+    return render_template("start_todos.html", liste=neue_liste)
 
 @app.route("/add", methods=["GET", "POST"])
 def add_new_todo():
