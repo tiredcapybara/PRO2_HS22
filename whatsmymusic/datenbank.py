5f005d1441cd481839898de9aaeefb9fdf8b9cb8
tiredcapybara <113971222+tiredcapybara@users.noreply.github.com>
2022-11-29 17:11:41 +0100

    Ausarbeitung Filtern + erste Arbeiten Bearbeitung

--- whatsmymusic/datenbank.py
+++ whatsmymusic/datenbank.py
@@ -3,58 +3,80 @@ import json
 
 def auslesen():
     file = open("databasesongs.json")
-    songs = json.load(file)
-    return songs
+    eintraege = json.load(file)
+    return eintraege
 
 
-def song_speichern(daten):
-    songs = auslesen()
-    id_song = songs[-1]["id"]
+def speichern(daten):
+    eintraege = auslesen()
+    id_eintrag = eintraege[-1]["id"]
 
-    song = {
-        "id": id_song + 1,
+    eintrag = {
+        "id": id_eintrag + 1,
         "titel": daten["titel"],
         "intepret": daten["intepret"],
         "genre": daten["genre"],
         "gehoert": daten["gehoert"],
         "release": daten["release"],
-        "rating": daten["rating"]
+        "rating": daten["rating"],
+        "typ": daten["typ"]
     }
-    songs.append(song)
-    print(songs)
+    eintraege.append(eintrag)
+    print(eintraege)
 
-    songs_json = json.dumps(songs, indent=4)
+    eintraege_json = json.dumps(eintraege, indent=4)
     file = open("databasesongs.json", "w")
-    file.write(songs_json)
+    file.write(eintraege_json)
     file.close()
     return
+def sortiert_eintraege(merkmale):
+    eintraege = auslesen()
+    eintraege_gefiltert = []
+    for eintrag in eintraege:
+        checks = (
+            merkmale["typ"] == "" or eintrag["typ"] == merkmale["typ"],
+            merkmale["intepret"] == "" or eintrag["intepret"] == merkmale["intepret"],
+            merkmale["release"] == "" or eintrag["release"] == merkmale["release"],
+            merkmale["gehoert"] == "" or eintrag["gehoert"] == merkmale["gehoert"],
+            merkmale["rating"] == "" or eintrag["rating"] == merkmale["rating"]
+        )
+        if all(checks):
+            eintraege_gefiltert.append(eintrag)
+    return eintraege_gefiltert
 
+def auslesen_ausgewaehlt(eintrag_id):
+    for eintrag in auslesen():
+        if eintrag["id"] == eintrag_id:
+            return eintrag
+    return
 
-def auslesenalb():
-    file = open("databasealb.json.")
-    alben = json.load(file)
-    print(alben)
-    return alben
-
-
-def album_speichern(datenalb):
-    alben = auslesenalb()
-    id_album = alben[-1]["id"]
-
-    album = {
-        "id": id_album + 1,
-        "titel": datenalb["albtitel"],
-        "intepret": datenalb["albintepret"],
-        "genre": datenalb["albgenre"],
-        "gehoert": datenalb["albgehoert"],
-        "release": datenalb["albrelease"],
-        "rating": datenalb["albrating"]
+def eintrag_korrigiert(eintrag_id, daten):
+    position = 0
+    eintraege = auslesen()
+    eintrag_bearbeitet = {
+        "id": eintrag_id,
+        "titel": daten["titel"],
+        "intepret": daten["intepret"],
+        "genre": daten["genre"],
+        "gehoert": daten["gehoert"],
+        "release": daten["release"],
+        "rating": daten["rating"],
+        "typ": daten["typ"]
     }
-    alben.append(album)
-    print(album)
 
-    alben_json = json.dumps(alben, indent=4)
-    file = open("databasealb.json", "w")
-    file.write(alben_json)
+    for eintrag in eintraege:
+        if eintrag["id"] == eintrag_id:
+            print(eintrag["id"])
+            print(position)
+            eintraege[position] = eintrag_bearbeitet
+        else:
+            position = position + 1
+
+
+    eintraege_json = json.dumps(eintraege, indent=4)
+    file = open("databasesongs.json", "w")
+    file.write(eintraege_json)
     file.close()
-    return
+    return auslesen_ausgewaehlt(eintrag_id)
+
+
--- whatsmymusic/main.py
+++ whatsmymusic/main.py
@@ -1,6 +1,6 @@
 # Importe
 from flask import Flask, render_template, request, redirect
-from whatsmymusic.datenbank import auslesen, auslesenalb, album_speichern, song_speichern
+from whatsmymusic.datenbank import auslesen, speichern, sortiert_eintraege, auslesen_ausgewaehlt, eintrag_korrigiert
 
 app = Flask("whatsmymusic")
 
@@ -11,53 +11,48 @@ def index():
     return render_template("index.html")
 
 
-# Route Album-Eintrag erstellen
-@app.route("/neuer-eintrag-album", methods=["GET", "POST"])
-def neualbum():
+# Route Eintrag erstellen
+@app.route("/neuer-eintrag", methods=["GET", "POST"])
+def neueintrag():
     if request.method == "GET":
-        return render_template("neuer-eintrag-album.html")
+        return render_template("neuer-eintrag.html")
 
     if request.method == "POST":
-        daten_alben = request.form.to_dict()
-        album_speichern(daten_alben)
-        print(daten_alben)
-        return redirect("/archiv")
-
-
-# Route Song-Eintrag erstellen
-@app.route("/neuer-eintrag-song", methods=["GET", "POST"])
-def neusong():
-    if request.method == "GET":
-        return render_template("neuer-eintrag-song.html")
-
-    if request.method == "POST":
-        daten_songs = request.form.to_dict()
-        song_speichern(daten_songs)
+        daten_eintraege = request.form.to_dict()
+        speichern(daten_eintraege)
         return redirect("/archiv")
 
 
 # Route Archiv öffnen
-@app.route("/archiv")
+@app.route("/archiv", methods=["GET", "POST"])
 def archivopen():
+    typ = ["Lied", "Album"]
     genre = ["Rock", "Rap"]
-    return render_template("archiv.html", genres=genre)
-
-    # Wie kann ich zwischen Alben und Songs unterscheiden? If Checkmark1 == Songs?
-    #if request.method == "GET":
-      #  return render_template("archiv.html")
-    #if request.method == "POST":
-
-#songs = auslesen()
-#   selection = []
-  #  for song in songs:
- #       if song["genre"] == "Rock":
- #           selection.append(song)
+    intepret = ["Declan McKenna", "Eminem", "Lana Del Rey"]
+    monat = ["11-2022", "10-2022", "09-2022"]
+    jahr = [2002, 2015, 2022]
+    bewertung = [1, 2, 3, 4, 5]
+    if request.method == "GET":
+        eintraege = auslesen()
+    if request.method == "POST":
+        eintraege = sortiert_eintraege(request.form.to_dict())
+    return render_template("archiv.html", typen=typ, genres=genre, intepreten=intepret, monate=monat, jahre=jahr,
+                               bewertungen=bewertung, eintraege=eintraege)
 
 
-@app.route("/bearbeiten")
-def eintragbearbeiten():
-    return render_template("bearbeiten.html")
 
+# Route Einträge bearbeiten
+@app.route("/bearbeiten/<eintrag_id>", methods=["GET", "POST"])
+def eintragbearbeiten(eintrag_id):
+    if request.method == "GET":
+        eintrag = auslesen_ausgewaehlt(int(eintrag_id))
+        return render_template("bearbeiten.html", eintrag=eintrag)
+    if request.method == "POST":
+        eintrag_korrigiert(int(eintrag_id), request.form.to_dict())
+        return redirect("/archiv")
+
+
+# Route Datenvisualisierung
 @app.route("/statistiken")
 def statistikopen():
     return render_template("statistiken.html")
