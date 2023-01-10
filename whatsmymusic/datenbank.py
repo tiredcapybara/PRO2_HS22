644c5de8fbd59f86bd7b5b9aac115430e96defdd
tiredcapybara <113971222+tiredcapybara@users.noreply.github.com>
2023-01-10 18:10:26 +0100

    Kommentare

--- whatsmymusic/datenbank.py
+++ whatsmymusic/datenbank.py
@@ -1,20 +1,21 @@
-#Importe für Datenspeicherung, Datumsformate und Balkendiagramm zeichnen
+# Importe für Datenspeicherung, Datumsformate und Balkendiagramm zeichnen
 import json
 from datetime import datetime
 import plotly.express as px
 from plotly.offline import plot
 
-#Funktion um Einträge auszulesen aus json File
+
+# Funktion um Einträge auszulesen aus json File
 def auslesen():
     file = open("databasesongs.json")
-    #Variable eintraege wird dem Inhalt des json Files gleichgesetzt
+    # Variable eintraege wird dem Inhalt des json Files gleichgesetzt
     eintraege = json.load(file)
-    #Einträge werden ausgegeben
     return eintraege
+
 
 # Neue Einträge abspeichern
 def speichern(daten):
-    #Variable eintraege wird Daten aus File gleichgesetzt
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
     # id erstellen um Einträge wiederzufinden für löschen und bearbeiten. Einzigartig für jeden Eintrag
     id_eintrag = eintraege[-1]["id"]
@@ -29,24 +30,29 @@ def speichern(daten):
         "rating": daten["rating"],
         "typ": daten["typ"]
     }
-    #Neuer Eintrag wird am Ende des Dictionary angehängt
+    # Neuer Eintrag wird am Ende des Dictionary angehängt
     eintraege.append(eintrag)
-    #json formatieren
+    # json formatieren
     eintraege_json = json.dumps(eintraege, indent=4)
-    #json file wird verändert
+    # json file wird verändert
     file = open("databasesongs.json", "w")
     file.write(eintraege_json)
     file.close()
     return
 
+
 # Filterungsfunktion für Einträge
 def sortiert_eintraege(merkmale):
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
+    # Liste der Einträge nach Filtern, zur Ausgabe
     eintraege_gefiltert = []
     # Checks für Übereinstimmung der Filterung
     for eintrag in eintraege:
+        # Hördatum wird in Schema Jahr, Monat und Tag festgehalten, um Jahr und Monat auszupicken
         date = eintrag["gehoert"]
         date = datetime.strptime(date, "%Y-%m-%d")
+        # Checks um die Einträge
         checks = (
             merkmale["typ"] == "" or eintrag["typ"] == merkmale["typ"],
             merkmale["intepret"] == "" or eintrag["intepret"] == merkmale["intepret"],
@@ -58,18 +64,25 @@ def sortiert_eintraege(merkmale):
         )
         if all(checks):
             eintraege_gefiltert.append(eintrag)
+    # Gefilterte Einträge werden ausgegeben
     return eintraege_gefiltert
 
+
 def auslesen_ausgewaehlt(eintrag_id):
+    # Hiermit wird die id von ausgewählten Einträgen eingelesen und kann denn richtigen Eintrag ausgeben. Für bearbeiten und löschen benötigt
     for eintrag in auslesen():
         if eintrag["id"] == eintrag_id:
             return eintrag
     return
 
+
 # Veränderungen in Beiträgen festhalten
 def eintrag_korrigiert(eintrag_id, daten):
+    # Variable position für die Suche nach richtiger id
     position = 0
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
+    # Aufbau bearbeiteter Eintrag (gleich wie der normale Eintrag im file)
     eintrag_bearbeitet = {
         "id": eintrag_id,
         "titel": daten["titel"],
@@ -80,68 +93,81 @@ def eintrag_korrigiert(eintrag_id, daten):
         "rating": daten["rating"],
         "typ": daten["typ"]
     }
-    # Erstellung ids für Einträge
+    # Die ids im file werden alle durch-kontrolliert bis man zum id des zu bearbeitenden Eintrages kommt
     for eintrag in eintraege:
         if eintrag["id"] == eintrag_id:
-            print(eintrag["id"])
-            print(position)
             eintraege[position] = eintrag_bearbeitet
         else:
             position = position + 1
 
-    # Auf Daten in json zugreifen
+    # Auf Daten in json zugreifen und änderungen abgeben
     eintraege_json = json.dumps(eintraege, indent=4)
     file = open("databasesongs.json", "w")
     file.write(eintraege_json)
     file.close()
     return auslesen_ausgewaehlt(eintrag_id)
 
-# JSON Liste anpassen nach Löschung
+
+# json anpassen nach Löschung
 def eintrag_loeschen(eintrag_id):
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
+    # Leere Liste für die nicht-gelöschten Einträge
     eintraege_neu = []
     for eintrag in eintraege:
+        # Es wird kontrolliert, ob dieser Eintrag der zu löschende Eintrag ist. Falls es dieser NICHT ist wird der Beitrag in die erstellte Liste hinzugefügt
         if eintrag["id"] != eintrag_id:
             eintraege_neu.append(eintrag)
+    # Die Daten aus der werden zum json file hinzugefügt. Der gelöschte Beitrag ist nicht in der Liste ergo verschwindet dieser
     eintraege_json = json.dumps(eintraege_neu, indent=4)
     file = open("databasesongs.json", "w")
     file.write(eintraege_json)
     file.close()
     return
 
-# Funktion für Aufteilung Datum in Monat und Jahr
+
+# Funktion für Aufteilung Datum in Monat und Jahr in Liste
 def liste_gehoert():
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
+    # Leere Listen werden erstellt, um Daten zu Monaten und Jahren zu speichern
     gehoert_monate = []
     gehoert_jahre = []
     for eintrag in eintraege:
+        # Jeder einzelne Eintrag wird kontrolliert, das Hördatum ausgewählt und in das richtige Format gespeichert
         date = eintrag["gehoert"]
         date = datetime.strptime(date, "%Y-%m-%d")
+        # Monat wird ausgepickt und in die Liste eingetragen, Doppelnennungen werden nicht berrücksichtigt
         if date.month not in gehoert_monate:
             gehoert_monate.append(date.month)
+        # Jahr wird ausgepickt und in die Liste eingetragen, Doppelnennungen werden nicht berrücksichtigt
         if date.year not in gehoert_jahre:
             gehoert_jahre.append(date.year)
-
+    # Listen werden sortiert
     gehoert_monate = sorted(gehoert_monate)
     gehoert_jahre = sorted(gehoert_jahre)
-
+    #Die Listen werden im Dictionary resultat eingetragen
     resultat = {
         "monate": gehoert_monate,
         "jahre": gehoert_jahre
     }
     return resultat
 
-#Funktion für das Zeichnen des Diagramms
+
+# Funktion für das Zeichnen des Diagramms
 def statistik_zeichnen(auswahl_x):
-    #Variable eintraege wird Daten aus File gleichgesetzt
+    # Variable eintraege wird Daten aus File gleichgesetzt
     eintraege = auslesen()
-    #Leeres Dictionary für Diagramm-Daten wird erstellt
+    # Leeres Dictionary für Diagramm-Daten wird erstellt
     ratings = {}
     for eintrag in eintraege:
+        # Es wird durch jeden Eintrag gegangen und alle jeweiligen Ratings rausgenommen. Die gleichen Ratings werden gezählt
+        # die Anzahl von einem jeweiligen Rating wird als Value im Dictionary festgehalten
         if eintrag["rating"] not in ratings:
             ratings[eintrag["rating"]] = 1
         else:
             ratings[eintrag["rating"]] += 1
+    # Wiederholung des gleichen Prinzips
     genres = {}
     for eintrag in eintraege:
         if eintrag["genre"] not in genres:
@@ -154,20 +180,29 @@ def statistik_zeichnen(auswahl_x):
             intepreten[eintrag["intepret"]] = 1
         else:
             intepreten[eintrag["intepret"]] += 1
+    releases = {}
+    for eintrag in eintraege:
+        if eintrag["release"] not in releases:
+            releases[eintrag["release"]] = 1
+        else:
+            releases[eintrag["release"]] += 1
 
-    #Check, welche Daten für Diagramm ausgewählt wurden
+    # Check, welche Daten für Diagramm ausgewählt wurden
     if auswahl_x == "rating":
-        #Das erstellte Dictionary wird in Keys und Values aufgeteilt. X-Achse = keys Y-Achse = values (diese sind je nach Diagramm verschieden)
+        # Das erstellte Dictionary wird in Keys und Values aufgeteilt. X-Achse = keys Y-Achse = values (diese sind je nach Diagramm verschieden)
         x = ratings.keys()
         y = ratings.values()
     elif auswahl_x == "genre":
         x = genres.keys()
         y = genres.values()
+    elif auswahl_x == "release":
+        x = releases.keys()
+        y = releases.values()
     else:
         x = intepreten.keys()
         y = intepreten.values()
 
-    #Diagramm zeichnen
+    # Diagramm zeichnen
     fig = px.bar(x=x, y=y)
     div = plot(fig, output_type="div")
     return div
--- whatsmymusic/main.py
+++ whatsmymusic/main.py
@@ -5,35 +5,38 @@ from whatsmymusic.datenbank import auslesen, speichern, sortiert_eintraege, ausl
 
 app = Flask("whatsmymusic")
 
+
 # Route Startseite
 @app.route("/")
 def index():
-    #HTML der Startseite wird angezeigt
+    # HTML der Startseite wird angezeigt
     return render_template("index.html")
+
 
 # Route Eintrag erstellen
 @app.route("/neuer-eintrag", methods=["GET", "POST"])
 def neueintrag():
-    #HTML wird angezeigt bei Öffnen
+    # HTML wird angezeigt bei Öffnen
     if request.method == "GET":
         return render_template("neuer-eintrag.html")
-    #Bei Erstellen von neuem Eintrag werden Daten gespeichert und man wird zm Archiv weitergeleitet
+    # Bei Erstellen von neuem Eintrag werden Daten gespeichert und man wird zm Archiv weitergeleitet
     if request.method == "POST":
         daten_eintraege = request.form.to_dict()
         speichern(daten_eintraege)
         return redirect("/archiv")
 
+
 # Route Archiv
 @app.route("/archiv", methods=["GET", "POST"])
 def archivopen():
-    #Die verschiedenen Filtermöglichkeiten als Listen formatieren, hier werden alle Einträge ausgelesen und
-    #die Listen mit nur einzigartigen Einträgen erstellt um nachher diese in den Auswahlfelder angezeigt zu kriegen
+    # Die verschiedenen Filtermöglichkeiten als Listen formatieren, hier werden alle Einträge ausgelesen und
+    # die Listen mit nur einzigartigen Einträgen erstellt um nachher diese in den Auswahlfelder angezeigt zu kriegen
     eintraege = auslesen()
-    #Intepret aus jedem Eintrag wird in eine Liste hinzugefügt
+    # Intepret aus jedem Eintrag wird in eine Liste hinzugefügt
     liste_intepreten = [resultat['intepret'] for resultat in eintraege]
-    #Doppeltnennungen werden gelöscht
+    # Doppeltnennungen werden gelöscht
     liste_intepreten = set(liste_intepreten)
-    #Liste wird sortiert. Das gleiche passiert auch für die unteren 2 Listen
+    # Liste wird sortiert. Das gleiche passiert auch für die unteren 2 Listen
     liste_intepreten = sorted(liste_intepreten)
 
     liste_genre = [resultat['genre'] for resultat in eintraege]
@@ -44,7 +47,7 @@ def archivopen():
     liste_release = set(liste_release)
     liste_release = sorted(liste_release)
 
-    #Auswahlmöglichkeiten Select Filter erstellen
+    # Auswahlmöglichkeiten Select Filter erstellen
     typ = ["Lied", "Album"]
     genre = liste_genre
     intepret = liste_intepreten
@@ -53,51 +56,61 @@ def archivopen():
     jahr = liste_release
     bewertung = ["1", "2", "3", "4", "5"]
 
-    #GET Methode zeigt es automatisch alle Einträge an (also wenn man das Archiv einfach nur öffnet
+    # GET Methode zeigt es automatisch alle Einträge an (also wenn man das Archiv einfach nur öffnet
     if request.method == "GET":
         eintraege = auslesen()
-    #Nach der Auswahl der Filter werden nur die Einträge angezeigt, welche noch im gefilterten Dictionary sind, also den ausgewählten Kriterien ensprechen
+    # Nach der Auswahl der Filter werden nur die Einträge angezeigt, welche noch im gefilterten Dictionary sind, also den ausgewählten Kriterien ensprechen
     if request.method == "POST":
         eintraege = sortiert_eintraege(request.form.to_dict())
     return render_template("archiv.html", typen=typ, genres=genre, intepreten=intepret, monate=monat,
                            hoerjahre=hoerjahr, jahre=jahr, bewertungen=bewertung, eintraege=eintraege)
 
+
 # Route Einträge bearbeiten. Eintrag wird mittels id angezeigt
 @app.route("/bearbeiten/<eintrag_id>", methods=["GET", "POST"])
 def eintragbearbeiten(eintrag_id):
+    # Ausgewählter Eintrag wird angezeigt in Bearbeitungs-Maske
     if request.method == "GET":
         eintrag = auslesen_ausgewaehlt(int(eintrag_id))
         return render_template("bearbeiten.html", eintrag=eintrag)
+    # Änderungen werden in json file gespeichert
     if request.method == "POST":
         eintrag_korrigiert(int(eintrag_id), request.form.to_dict())
         return redirect("/archiv")
 
+
 # Route Eintraege löschen
 @app.route("/loeschen/<eintrag_id>", methods=["GET", "POST"])
 def eintragloeschenopen(eintrag_id):
+    # Bei Klick auf Delete-Button wird der Eintrag mittels id gelöscht.
     eintrag_loeschen(int(eintrag_id))
     return redirect("/archiv")
+
 
 # Route Statistiken
 @app.route("/statistiken", methods=["GET", "POST"])
 def grafik():
-    #Durch Diagramm-Titel wird das h3 im html erstellt, um das Diagramm besser deuten zu können
-    #Bei Öffnen der Seite sieht man nur die 2 Selection-Balken
+    # Durch Diagramm-Titel wird das h3 im html erstellt, um das Diagramm besser deuten zu können
+    # Bei Öffnen der Seite sieht man nur die 2 Selection-Balken
     if request.method == "GET":
         return render_template("statistiken.html", diagramm_titel="false")
-    #Nach Auswahl wird kontrolliert, was als x-Achse ausgewählt wurde
+    # Nach Auswahl wird kontrolliert, was als x-Achse ausgewählt wurde
     if request.method == "POST":
-        #Es wird ausgewählt, welches Diagramm angezeigt wird
+        # Es wird ausgewählt, welches Diagramm angezeigt wird (basierend auf Auswahl)
         auswahl_x = request.form.to_dict()["x-achse"]
         div = statistik_zeichnen(auswahl_x)
-        #Diagramm-Titel wird angepasst
+        # Diagramm-Titel wird angepasst
         if auswahl_x == "rating":
             diagramm_titel = "Bewertungen"
         elif auswahl_x == "intepret":
             diagramm_titel = "Intepreten"
+        elif auswahl_x == "release":
+            diagramm_titel = "Erscheinungsjahre"
         else:
             diagramm_titel = "Genres"
+        # Diagramm wird angezeigt
         return render_template("statistiken.html", barchart=div, diagramm_titel=diagramm_titel)
+
 
 # Run App
 if __name__ == "__main__":
